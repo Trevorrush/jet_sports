02ea6f156cb6fbe45e28568b59b1456211124da5
Raymond Chen <iamraymondc@gmail.com>
2015-07-15 15:12:36 -0700

    Added download button

--- app/src/main/java/com/travel/reconsports/MainActivity.java
+++ app/src/main/java/com/travel/reconsports/MainActivity.java
@@ -2,13 +2,20 @@ package com.travel.reconsports;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity{
+
+    private Button downloadButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //  Setup button
+        downloadButton = (Button) findViewById(R.id.tempDownloadButton) ;
+        
     }
 
 }
